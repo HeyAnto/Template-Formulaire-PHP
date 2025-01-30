b39c344b0cc6797a1f750dc5527f2cfe40c2b2d5
HeyAnto <antonin63170@gmail.com>
2025-01-30 22:27:27 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Formulaire Contact PHP</title>
+
+    <link rel="icon" type="image/svg+xml" href="assets/favico.svg">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
